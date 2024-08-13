da11f09ecc7aeb626dab543fa940a95f58121408
sc-jstribling <jstribling@squareandcompassgames.com>
2024-08-13 15:15:13 -0500

    chore: initial code commit

---
+++ Source/BeamExample/BeamExampleGameModeBase.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeamExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEAMEXAMPLE_API ABeamExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BeamExample/BeamExampleModule.cpp
@@ -0,0 +1,4 @@
+#include "BeamExampleModule.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BeamExampleModule, "BeamExampleModule" );
