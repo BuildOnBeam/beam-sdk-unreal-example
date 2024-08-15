1d6ff6d933d8f1329fe5cce7d37cbb1da7d9c859
sc-jstribling <jstribling@squareandcompassgames.com>
2024-08-15 01:42:31 -0500

    feat: update BeamClient config methods and add default storage, add BeamStatus to show initial SDK status in UI

--- Source/BeamExample/ExampleGameInstance.cpp
+++ Source/BeamExample/ExampleGameInstance.cpp
@@ -2,20 +2,30 @@
 
 DEFINE_LOG_CATEGORY_STATIC(LogBeamExampleGameInstance, Log, All);
 
-void UExampleGameInstance::Init() {
+void UExampleGameInstance::Init()
+{
 	Super::Init();
 	
-	if (BeamApiKey.IsEmpty()) {
+	if (BeamApiKey.IsEmpty())
+	{
 		// Note: You can invent your own system for injecting the BeamApiKey, usage of a Secret.ini is just to keep
 		//    the BeamApiKey out of the source code and source control.
 		UE_LOG(LogBeamExampleGameInstance, Error,
 			TEXT("%hs: BeamApiKey not set, create a DefaultSecrets.ini in the Config folder with the "
 			"[/Script/BeamExample.ExampleGameInstance] section and entry of BeamApiKey=YOUR_API_KEY"),
 			__func__);
+		BeamStatus = "Error - Missing Beam API Key";
+	}
+	else
+	{
+		BeamStatus = "Ready";
 	}
 
-	BeamClient = NewObject<UBeamClient>(this);
-	BeamClient->SetBeamApiKey(BeamApiKey)
-	.SetEnvironment(EBeamEnvironment::Testnet)
-	.SetDebugLogging(true);
+	BeamStorage = UBeamSaveGameStorage::LoadOrCreate();
+	BeamClient = NewObject<UBeamClient>(this)
+		->SetBeamApiKey(BeamApiKey)
+		->SetEnvironment(EBeamEnvironment::Testnet)
+		->SetDebugLogging(true)
+		->SetStorage(BeamStorage);
+	BeamStorage->Save();
 }
--- Source/BeamExample/ExampleGameInstance.h
+++ Source/BeamExample/ExampleGameInstance.h
@@ -1,11 +1,13 @@
 ﻿#pragma once
 
 #include "BeamClient.h"
+#include "BeamSDK/Public/Storage/BeamSaveGameStorage.h"
 #include "Engine/GameInstance.h"
 #include "ExampleGameInstance.generated.h"
 
 UCLASS(config=Secrets)
-class BEAMEXAMPLE_API UExampleGameInstance : public UGameInstance {
+class BEAMEXAMPLE_API UExampleGameInstance : public UGameInstance
+{
 	GENERATED_BODY()
 	
 protected:
@@ -14,7 +16,13 @@ protected:
 public:
 	UPROPERTY(config)
 	FString BeamApiKey;
+
+	UPROPERTY(BlueprintReadOnly, Category="Beam")
+	FString BeamStatus;
 	
-	UPROPERTY()
-	UBeamClient* BeamClient;
+	UPROPERTY(BlueprintReadOnly, Category="Beam")
+	UBeamSaveGameStorage* BeamStorage = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category="Beam")
+	UBeamClient* BeamClient = nullptr;
 };
