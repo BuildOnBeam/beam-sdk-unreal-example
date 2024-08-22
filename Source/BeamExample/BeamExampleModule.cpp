0b40aae88c6ed1ae9a4bd6565b4b2e4df048c3b7
Eric Buxkemper <ericbuxkemper@gmail.com>
2024-08-22 11:54:33 -0500

    fix: rename example module so packaging works

--- Source/BeamExample/BeamExampleModule.cpp
+++ Source/BeamExample/BeamExampleModule.cpp
@@ -1,4 +1,4 @@
 #include "BeamExampleModule.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BeamExampleModule, "BeamExampleModule" );
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BeamExample, "BeamExample" );
